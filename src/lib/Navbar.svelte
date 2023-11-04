d519ccac8bb13ff8c3c38788349be62ee431c0c3
Zerkath <juha.airaksinen1@gmail.com>
2023-11-04 17:45:38 +0200

    feat: Sidebar
    - Added sidebar with no functionality
    - Currently some issue stopping events from propagating to child
      elements

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,69 @@
+<script lang="ts" context="module">
+  import PlaceholderIcon from './PlaceholderIcon.svelte';
+  import NavbarOptionText from './NavbarOptionText.svelte';
+
+  let isCollapsed = false;
+
+  const collapse = () => {
+    isCollapsed = !isCollapsed;
+    console.log(isCollapsed);
+  };
+</script>
+
+<div class="navbar">
+  <div class="navbar__option">
+    <PlaceholderIcon content="1" />
+    <NavbarOptionText
+      header="Listing"
+      desc="List expenses, do modifications and removals"
+      isCollapsed={isCollapsed}
+    />
+  </div>
+  <div class="navbar__option">
+    <PlaceholderIcon content="2" />
+    <NavbarOptionText
+      header="Reports"
+      desc="View visualized aggregate information about transactions"
+      isCollapsed={isCollapsed}
+    />
+  </div>
+  <div class="navbar__option">
+    <PlaceholderIcon content="3" />
+    <NavbarOptionText
+      header="Categories & Transactions"
+      desc="Add new transactions, modify, add or remove categories"
+      isCollapsed={isCollapsed}
+    />
+  </div>
+  <div class="navbar__spacer navbar__landscape" />
+  <div class="navbar__option navbar__landscape" on:click={collapse}>
+    <PlaceholderIcon content="4"/>
+    <NavbarOptionText
+      header="Collapse"
+      desc="Collapse the sidebar"
+      isCollapsed={isCollapsed}
+    />
+  </div>
+  <div class="navbar__option">
+    <PlaceholderIcon content="5" />
+    <NavbarOptionText
+      header="Options"
+      desc="Application options"
+      isCollapsed={isCollapsed}
+    />
+  </div>
+</div>
+
+<style lang="scss">
+  .navbar__spacer {
+    height: 100%;
+  }
+
+  .navbar__option {
+    margin: 10px;
+    display: flex;
+    align-items: center;
+    width: fit-content;
+    flex-direction: row;
+  }
+</style>
---
+++ src/lib/NavbarOptionText.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+  export let header: string;
+  export let desc: string;
+  export let isCollapsed: boolean = false;
+</script>
+
+<div class="navbar__text">
+  <span class="navbar__text__header">{header}</span>
+  <span class="navbar__text__desc">{desc}</span>
+  <span class="navbar__text__desc">{isCollapsed}</span>
+</div>
+
+<style lang="scss">
+  .navbar__text {
+    margin: 0 10px;
+    display: flex;
+    flex-direction: column;
+    margin-right: 10px;
+  }
+
+  .navbar__text__header {
+    font-size: 1.1rem;
+  }
+
+  .navbar__text__desc {
+    font-size: 0.7rem;
+    font-style: italic;
+    color: #555;
+  }
+</style>
