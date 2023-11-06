68d0140873dc76b31f253775d44c72ff084e5b54
Zerkath <24889977+Zerkath@users.noreply.github.com>
2023-11-06 17:54:08 +0200

    feat: Inputs for report view
    - Moved App to lib folder
    - Formatted files

--- src/lib/Reports.svelte
+++ src/lib/Reports.svelte
@@ -1,1 +1,74 @@
-<p>Reports</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { invoke } from '@tauri-apps/api/tauri';
+
+  let date = new Date().toISOString().slice(0, 10);
+
+  let reportOptions: string[] = [];
+
+  type Report = {
+    total: number;
+
+  };
+
+  let data: Report | undefined = undefined;
+
+  onMount(() => {
+    invoke('get_report_types').then((res) => {
+      if (Array.isArray(res)) {
+        reportOptions = res;
+      }
+    });
+  });
+</script>
+
+<section>
+  <div class="report__controls">
+    <input type="date" placeholder="Select Date" bind:value={date} />
+    <select>
+      {#each reportOptions as option}
+        <option value={option}>{option.toLowerCase()}</option>
+      {/each}
+    </select>
+    <button>Load</button>
+  </div>
+
+  {#if data != undefined}
+    <div class="report__data">
+      <h2>Total: {data.total}</h2>
+    </div>
+  {:else}
+    <div class="report__data">
+      <span>No report loaded</span>
+    </div>
+  {/if}
+
+</section>
+
+<style lang="scss">
+  section {
+    margin: 1rem 0;
+  }
+  .report__controls {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .report__controls > select, input {
+    margin: 0 6px;
+    width: 160px;
+  }
+
+  .report__data {
+    display: flex;
+    flex-direction: column;
+    margin: auto;  
+    align-items: center;
+  }
+
+  option {
+    // uppercase the first letter
+    text-transform: capitalize;
+  }
+</style>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,3 @@
-
 export type Option = 'listing' | 'reports' | 'categories' | 'options';
 
 export type Category = {
