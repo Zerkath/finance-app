a7f195ae5981674d5a0d92e3406bce5ffb2e01cc
Zerkath <juha.airaksinen1@gmail.com>
2023-11-09 23:01:16 +0200

    feat: Added Datavisualization for reports with D3
    - Treemap for categories
    - Lollipop graph for date expenses

--- src/main.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from 'vitest';
-
-// Placeholder test to pass the test phase, as no tests returns a non-zero exit code
-test('placeholder', () => {
-  expect(true).toBe(true);
-});
